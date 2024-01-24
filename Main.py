d89b710431e3550ab55b4b81bd7745e0fbbbce3e
moduloooo <86678091+mridg@users.noreply.github.com>
2024-01-23 22:09:28 -0500

    1/23/24

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-import matplotlib as math
+import matplotlib as plt
+import matplotlib.pyplot as plt
+
 
 # def forwardprop(weight, bias, input):
 #     z = weight.dot(input) + bias # issue with the dot product
@@ -16,8 +18,7 @@ def setup():
 
     return w1, b1, w2, b2
 
-def forwardprop(A):
-    w1, b1, w2, b2 = setup()
+def forwardprop(w1, b1, w2, b2, A):
     z1 = ReLU(np.matmul(w1, A) + b1)
     z2 = sigmoid(np.matmul(w2, z1) + b2)
 
@@ -30,11 +31,13 @@ def backprop(w1, w2, b1, b2, z1, z2, input, ans, learningrate):
     w2f = w2 - learningrate * dw2
     b2f = b2 - learningrate * db2
 
-    dw1 = input * sigmoidprime(z1) * 2 * (w2 - w2f) #not sure check math on this 
+    dw1 = (z1 - b2) / w2 * sigmoidprime(z1) * 2 * (w2 - w2f) #not sure check math on this 
     db1 = 1 * sigmoidprime(z1) * 2 * (b2 - b2f) #check math not sure 
 
     w1f = w1 - learningrate * dw1
     b1f = b1 - learningrate * db1
+
+    return w1f, b1f, w2f, b2f
 
 def ReLU(input):
     return np.maximum(0, input)
@@ -56,52 +59,25 @@ def main():
     #print(data)
 
     #print(data.shape)
+    # xpoints = np.array([1, 8])
+    # ypoints = np.array([3, 10])
 
-    for i in range(10):
+    # plt.plot(xpoints, ypoints)
+    # plt.show()
+
+    w1, b1, w2, b2 = setup()
+    for i in range(100):
+        print(w2[1])
         input = np.delete(data[i].T , 0,  axis=0) #initial greyscale inputs
 
-        ans = np.zeros(10)
+        ans = np.zeros(100)
         ans[data[i][0]] = 1 #answer array for checking our results 
 
         #print(input.size)
-        print(forwardprop(input))
+        z1, z2 = forwardprop(w1, b1, w2, b2, input)
+        w1, b1, w2, b2 = backprop(w1, w2, b1, b2, z1, z2, input, ans, 0.01) #something about these matrices are being worn down
 
 
-
-        
-
-    # for i in range(len(data)):
-    #     forwardprop(data[i])
-    #     z1, z2 = forwardprop(i)
-    #     print(z2)
-
-
-
-
-    # ans = data[0] #can't use this form, we want this to be in binary so we can appropriately edit w and b
-
-    # a = np.empty([len(ans), 10])
-
-    # for i in range(len(ans)):
-    #     a[i, ans[i]] = 1
-
-    # w1, b1, w2, b2 = setup()
-
-    # learningrate = 0.5
-
-    # i = np.delete(data, 0, axis=0)
-
-    # for j in range(100):
-    #     z1, z2 = forwardprop(w1, b1, w2, b2, i)
-    #     dw1, db1, dw2, db2 = backwardprop(w2, i, z1, z2, a)
-
-    #     w1 = w1 - learningrate * dw1
-    #     b1 = b1 - learningrate * db1
-
-    #     w2 = w2 - learningrate * dw2
-    #     b2 = b2 - learningrate * db2
-
-    # print(z2)
 
 
 
