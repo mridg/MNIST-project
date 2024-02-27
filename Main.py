5a8bdbb3d4f43ef3d457f14e91bd552d4fcb227a
moduloooo <86678091+mridg@users.noreply.github.com>
2024-02-27 11:54:21 -0500

    final commit
    finished this project. a wonderful introduction to the world of neural networks.

--- Main.py
+++ Main.py
@@ -5,36 +5,22 @@ import matplotlib.pyplot as plt
 def setup():
     # first layer
     w1 = np.random.rand(20, 784) - 0.5
-    #w1 = np.random.randn(10, 784) #10 row 784 col
     b1 = np.random.rand(20, 1) - 0.5
-    #b1 = np.random.randn(10, 1) #10 row 1 col
     # second layer
     w2 = np.random.rand(10, 20) - 0.5
-    #w2 = np.random.randn(10, 10) #10 row 10 col
     b2 = np.random.rand(10, 1) - 0.5
-    #b2 = np.random.randn(10, 1) #10 row 1 col
 
     return w1, b1, w2, b2
 
 def forwardprop(w1, b1, w2, b2, A):
-    #z1 = sigmoid(np.matmul(w1, A) + b1) #10 row 1 col
     z1 = w1.dot(A) + b1
     a1 = sigmoid(z1)
     z2 = w2.dot(a1) + b2
     a2 = sigmoid(z2)
-    #z2 = sigmoid(np.matmul(w2, z1) + b2) #10 row 1 col
 
     return z1, a1, z2, a2
 
-#TODO: check if the code still work when the matrix dimensions change 
-#TODO: check if adding more layers will help !! unlikely but see what it does 
-#TODO: fun experiment, try learning rate scheduling
-
 def backprop(w1, w2, b1, b2, z1, z2, a1, a2, input, ans, learningrate):
-    #dw2 = np.matmul(np.matmul(z1, sigmoidprime(z2).T), (z2 - ans)) * 2 #10 row 1 col
-    #dw2 = 2 * (z2 - ans) @ sigmoidprime(w2 @ z1 + b2).T @ z1 # might be times z1?
-    #db2 = np.matmul((z2 - ans), sigmoidprime(z2)) * 2 #10 row 1 col
-    #db2 = 2 * (z2 - ans) * sigmoidprime(w2 @ z1 + b2) 
     m = 1 / len(input)
 
     dz2 = a2 - ans
@@ -43,14 +29,6 @@ def backprop(w1, w2, b1, b2, z1, z2, a1, a2, input, ans, learningrate):
 
     w2f = w2 - learningrate * dw2
     b2f = b2 - learningrate * db2
-
-    #dw1 = np.matmul(input, np.matmul(sigmoidprime(z1).T, (w2 - w2f))) * 2
-    #dw1 = (2 * (z2 - ans) * sigmoidprime(w2 @ z1 + b2) * z1 * sigmoidprime(w1 @ input + b1)) @ input.T
-    #dw1 = 2 * (z1 - (np.log(ans / (1 - ans))/ w2f)) @ sigmoidprime(w1 @ input + b1) @ input.T
-
-    #db1 = np.matmul(sigmoidprime(z1).T, (b2 - b2f)) * 2
-    #db1 = 2 * (z2 - ans) @ sigmoidprime(w2 @ z1 + b2).T @ sigmoidprime(w1 @ input + b1)
-    #db1 = 2 * (z1 - (np.log(ans / (1 - ans))/ w2f)) @ sigmoidprime(w1 @ input + b1)
 
     dz1 = w2.T.dot(dz2) * sigmoidprime(z1)
     dw1 = m * dz1.dot(input.T)
